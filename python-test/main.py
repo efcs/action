1659186975426b0ba09d3a4a14f135a5c743345d
eric <eric@efcs.ca>
2023-11-21 00:26:38 -0500

    attempt to reenable non-verbose output from LIT

--- python-test/main.py
+++ python-test/main.py
@@ -4,8 +4,28 @@ import rich
 rich.inspect(core)
 rich.print(core)
 
-core.error('Something went wrong.')
 core.save_state('time', 'new Date().toTimeString()')
 core.set_output('result', 'This is the result')
 core.info('Something went OK')
-core.set_failed('This went wrong.')
+
+
+from github import Github
+from actions_toolkit.github import Context
+
+context = Context()
+
+# First create a Github instance:
+g = Github(os.environ['GITHUB_TOKEN'])
+
+# Then play with your Github objects:
+repo = g.get_repo("efcs/action")
+
+commit = repo.get_commit(context.)
+
+check_run = repo.create_check_run(name="My Check Run",
+                                  head_sha=context.sha,
+                                  status="completed",
+                                  conclusion="success",
+                                  output={"title": "Check Run Output",
+                                           "summary": "Check run summary",
+                                           "text": "Check run detailed output"})
