ac2ff6cf2c58ee69209b42e6806f9b3776cebb52
eric <eric@efcs.ca>
2023-11-21 00:14:31 -0500

    test changes

--- python-test/main.py
+++ python-test/main.py
@@ -3,3 +3,9 @@ import actions_toolkit.core  as core
 import rich
 rich.inspect(core)
 rich.print(core)
+
+core.error('Something went wrong.')
+core.save_state('time', 'new Date().toTimeString()')
+core.set_output('result', 'This is the result')
+core.info('Something went OK')
+core.set_failed('This went wrong.')
