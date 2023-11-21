1c57e8b075269efc6c9f9647a7610a6a29a339cc
eric <eric@efcs.ca>
2023-11-21 11:34:13 -0500

    attempt check run

--- python-test/main.py
+++ python-test/main.py
@@ -1,16 +1,20 @@
 import actions_toolkit.core  as core
-
+from pydantic import BaseModel, Field, RootModel
+from typing import Literal, Any
 import rich
+import os
+from pathlib import Path
+import sys
+from github import Github
+from actions_toolkit.github import Context
+
+
 rich.inspect(core)
 rich.print(core)
 
 core.save_state('time', 'new Date().toTimeString()')
 core.set_output('result', 'This is the result')
 core.info('Something went OK')
-import os
-
-from github import Github
-from actions_toolkit.github import Context
 
 context = Context()
 
@@ -22,17 +26,8 @@ repo = g.get_repo("efcs/action")
 
 commit = repo.get_commit(context.sha)
 
-check_run = repo.create_check_run(name="My Check Run",
-                                  head_sha=context.sha,
-                                  status="completed",
-                                  conclusion="success",
-                                  output={"title": "Check Run Output",
-                                           "summary": "Check run summary",
-                                           "text": "Check run detailed output"})
 
 
-from pydantic import BaseModel, Field, RootModel
-from typing import Literal, Any
 
 class TestResult(BaseModel):
   code: Literal["PASS", "FAIL", "SKIP", "XPASS"]
@@ -46,11 +41,71 @@ class LITTestResults(BaseModel):
   elapsed: float
   tests: list[TestResult] = Field(default_factory=list)
 
-from pathlib import Path
-import sys
-import rich
+class Annotation(BaseModel):
+  path: str
+  start_line: int
+  end_line: int
+  annotation_level: Literal["notice", "warning", "failure"]
+  message: str
+  title: str
+  raw_details: str
+
+class CheckRun(BaseModel):
+  name: str
+  head_sha: str
+  status: Literal["queued", "in_progress", "completed"]
+  conclusion: Literal["success", "failure", "neutral", "cancelled", "timed_out", "action_required"]
+  output: dict[str, Any] = Field(default_factory=dict)
+  actions: list[dict[str, Any]] = Field(default_factory=list)
+  check_suite: dict[str, Any] = Field(default_factory=dict)
+  external_id: str
+  started_at: str
+  completed_at: str
+  output: dict[str, Any] = Field(default_factory=dict)
+  annotations: list[Annotation] = Field(default_factory=list)
+  pull_requests: list[dict[str, Any]] = Field(default_factory=list)
+
+
 rich.print(sys.argv)
 in_data = Path(sys.argv[1]).read_text()
 results = LITTestResults.model_validate_json(in_data)
-import rich
+
 rich.print(results)
+
+def process_results(results: LITTestResults):
+    annotations = []
+    conclusion = "success"
+
+    for test in results.tests:
+        if test.code == "FAIL":
+            conclusion = "failure"
+            path_name = test.name.split("::")[1].strip()
+            path = Path('libcxx/test', path_name)
+
+            annotation = {
+                "path": str(path),
+                "start_line": 1,
+                "end_line": 1,
+                "annotation_level": "failure",
+                "message": f"Test {test.name} FAILED",
+                "raw_details": test.output,
+                "title": "Test Failure"
+            }
+
+            annotations.append(annotation)
+
+    return conclusion, annotations
+
+
+annotations = conclusion, process_results(results)
+
+check_run = repo.create_check_run(name="Libc++ Test Suite",
+                                  head_sha=context.sha,
+                                  status="completed",
+                                  conclusion=conclusion,
+                                  output={"title": "Check Run Output",
+                                           "summary": f"{len(annotations)} tests failed.",
+                                           "annotations": annotations})
+
+
+
